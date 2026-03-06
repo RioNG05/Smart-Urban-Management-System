6ed84e3487659ea25c14aca15175f9d456487b9d
QuangAnh <qh1403@gmai.com>
2026-03-06 10:06:09 +0700

    Sửa hiển thị role trong api

--- backend/src/main/java/com/example/backend/Service/AccountService.java
+++ backend/src/main/java/com/example/backend/Service/AccountService.java
@@ -3,6 +3,7 @@ package com.example.backend.Service;
 import com.example.backend.DTO.Request.AccountCreateRequest;
 import com.example.backend.DTO.Request.AccountUpdateRequest;
 import com.example.backend.Entity.Account;
+import com.example.backend.Entity.Role;
 import com.example.backend.Repository.AccountRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -15,12 +16,14 @@ import java.util.List;
 public class AccountService {
     @Autowired
     private AccountRepository repository;
+    @Autowired
+    private RoleService roleService;
 
     public List<Account> findAll() {
         return repository.findAll();
     }
 
-    public Account findById(Integer id) {
+    public Account findById(java.lang.Integer id) {
         return repository.findById(id)
                 .orElseThrow(() -> new RuntimeException("Không tìm thấy Account với ID: " + id));
     }
@@ -29,32 +32,34 @@ public class AccountService {
         Account account =new Account();
         PasswordEncoder passwordEncoder = new BCryptPasswordEncoder(10);
 
-
         account.setEmail(req.getEmail());
         account.setUsername(req.getUsername());
         account.setPassword(passwordEncoder.encode(req.getPassword()));
-        account.setRole(req.getRole());
+        Role role = roleService.findById(req.getRoleId());
+        account.setRole(role);
         account.setIsActive(req.getActive());
 
         return repository.save(account);
     }
 
-    public Account update(Integer id, AccountUpdateRequest req) {
+    public Account update(java.lang.Integer id, AccountUpdateRequest req) {
         // Kiểm tra xem record có tồn tại không trước khi update
         Account account = findById(id);
 
         account.setEmail(req.getEmail());
         account.setUsername(req.getUsername());
         account.setPassword(req.getPassword());
-        account.setRole(req.getRole());
+        Role role = roleService.findById(req.getRoleId());
+        account.setRole(role);
         account.setIsActive(req.getActive());
         
-        return repository.save(account); // Tạm thời save đè (Cần cẩn thận nếu entity gửi lên thiếu trường)
+        return repository.save(account);
     }
 
-    public void delete(Integer id) {
+    public void delete(java.lang.Integer id) {
         // Kiểm tra tồn tại trước khi xóa
         findById(id); 
         repository.deleteById(id);
     }
+
 }
