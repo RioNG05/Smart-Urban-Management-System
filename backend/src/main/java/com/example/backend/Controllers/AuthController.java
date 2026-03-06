e500718ddfcda236fe5f090bfe62f1ce84b9e1ae
QuangAnh <qh1403@gmai.com>
2026-03-06 11:01:12 +0700

    fix

--- backend/src/main/java/com/example/backend/Controllers/AuthController.java
+++ backend/src/main/java/com/example/backend/Controllers/AuthController.java
@@ -3,7 +3,6 @@ package com.example.backend.Controllers;
 import com.example.backend.DTO.Request.LoginRequest;
 import com.example.backend.Entity.Account;
 import com.example.backend.Repository.AccountRepository;
-import com.example.backend.Security.JwtUtil;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -17,23 +16,23 @@ import java.util.Map;
 @RequestMapping("/api/auth")
 public class AuthController {
 
-    @Autowired
-    private AccountRepository repository;
-
-    @PostMapping("/login")
-    public Map<String, String> login(@RequestBody LoginRequest req){
-
-        Account account = repository.findByUsername(req.getUsername())
-                .orElseThrow(() -> new RuntimeException("User not found"));
-
-        BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
-
-        if(!encoder.matches(req.getPassword(), account.getPassword())){
-            throw new RuntimeException("Wrong password");
-        }
-
-        String token = JwtUtil.generateToken(account.getUsername());
-
-        return Map.of("token", token);
-    }
+//    @Autowired
+//    private AccountRepository repository;
+//
+//    @PostMapping("/login")
+//    public Map<String, String> login(@RequestBody LoginRequest req){
+//
+//        Account account = repository.findByUsername(req.getUsername())
+//                .orElseThrow(() -> new RuntimeException("User not found"));
+//
+//        BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
+//
+//        if(!encoder.matches(req.getPassword(), account.getPassword())){
+//            throw new RuntimeException("Wrong password");
+//        }
+//
+//        String token = JwtUtil.generateToken(account.getUsername());
+//
+//        return Map.of("token", token);
+//    }
 }
