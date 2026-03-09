0412aa37684312fdf4f6a3b6e121feccc2233636
DungDD2015 <dangducdung200105@gmail.com>
2026-03-09 10:13:06 +0700

    update responsive and footer

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,15 @@
 import AppRoutes from "./routers/AppRoutes";
+import { AuthProvider } from "./components/sections/auth/AuthContext";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 function App() {
-  return <AppRoutes />;
+  return (
+    <AuthProvider>
+      <AppRoutes />
+      <ToastContainer position="top-right" autoClose={2500} />
+    </AuthProvider>
+  );
 }
 
 export default App;
---
+++ frontend/src/pages/Unauthorized.jsx
@@ -0,0 +1,10 @@
+function Unauthorized() {
+  return (
+    <div style={{ textAlign: "center", marginTop: "100px" }}>
+      <h1>403</h1>
+      <p>You don't have permission to access this page</p>
+    </div>
+  );
+}
+
+export default Unauthorized;
