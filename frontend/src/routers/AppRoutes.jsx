6b403bd1d66c4c4771d7914cf4beea2cc8b0ffd3
RioNguyen <rionguyencontactwork@gmail.com>
2026-03-16 09:50:14 +0700

    Delete comment

--- frontend/src/routers/AppRoutes.jsx
+++ frontend/src/routers/AppRoutes.jsx
@@ -1,6 +1,4 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-
-// --- CÁC TRANG USER (GIỮ NGUYÊN) ---
 import MarketPage from "../pages/MarketPage";
 import AuthPage from "../pages/Authpage";
 import Product from "../pages/ProductDetailPage";
@@ -12,20 +10,9 @@ import Unauthorized from "../pages/Unauthorized";
 import Profile from "../pages/ProfilePage";
 import ServicePage from "../pages/ServicePage";
 import PrivateRoute from "./PrivateRoute";
-// --- IMPORT TỪ 4 FILE GỘP (GIỮ 100% CODE GỐC) ---
-// 1. Gộp Layout, Sidebar, Dashboard (Sử dụng Destructuring {})
 import { AdminLayout, AdminDashboard } from "../pages/admin/AdminCore";
-
-// 2. Gộp Role Manager và Lock Resident (Sử dụng Destructuring {})
-import {
-  AdminRoleManager,
-  AdminLockResident,
-} from "../pages/admin/AdminManagement";
-
-// 3. File Báo cáo (Default Import)
+import { AdminRoleManager, AdminLockResident } from "../pages/admin/AdminManagement";
 import AdminReports from "../pages/admin/AdminReports";
-
-// 4. File Khóa tài khoản lẻ (Default Import)
 import AdminAccountLock from "../pages/admin/AdminAccountLock";
 
 export default function AppRoutes() {
@@ -69,28 +56,15 @@ export default function AppRoutes() {
 
         <Route path="/unauthorized" element={<Unauthorized />} />
 
-        {/* --- HỆ THỐNG ADMIN (GHÉP CHUẨN TỪ CÁC FILE GỘP) --- */}
         <Route path="/admin" element={<AdminLayout />}>
-          {/* Trang chủ quản trị (Ảnh nhà cửa, Overview) */}
           <Route index element={<AdminDashboard />} />
-
-          {/* Quản lý quyền (Bảng Permissions Matrix) */}
           <Route path="roles" element={<AdminRoleManager />} />
-
-          {/* Quản lý cư dân (Banner tím, Form nhập) */}
           <Route path="lock-resident" element={<AdminLockResident />} />
-
-          {/* Các trang báo cáo và thống kê (Sử dụng chung AdminReports) */}
           <Route path="reports/revenue" element={<AdminReports />} />
           <Route path="reports/residents" element={<AdminReports />} />
           <Route path="reports/payments" element={<AdminReports />} />
           <Route path="reports/services" element={<AdminReports />} />
-
-          {/* Trang khóa tài khoản chung */}
-          <Route
-            path="account-lock"
-            element={<AdminAccountLock title="General Accounts" />}
-          />
+          <Route path="account-lock" element={<AdminAccountLock title="General Accounts" />} />
         </Route>
       </Routes>
     </BrowserRouter>
