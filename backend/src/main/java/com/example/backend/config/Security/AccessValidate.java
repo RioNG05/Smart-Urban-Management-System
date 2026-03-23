2766c2194bbbfba0ce7e5b5405567590395f7279
QuangAnh <qh1403@gmail.com>
2026-03-23 22:36:08 +0700

    Thêm authorization cho ResidentController

--- backend/src/main/java/com/example/backend/config/Security/AccessValidate.java
+++ backend/src/main/java/com/example/backend/config/Security/AccessValidate.java
@@ -2,8 +2,10 @@ package com.example.backend.config.Security;
 
 import com.example.backend.Entity.Account;
 import com.example.backend.Entity.Contract;
+import com.example.backend.Entity.Resident;
 import com.example.backend.Repository.ApartmentRepository;
 import com.example.backend.Repository.ContractRepository;
+import com.example.backend.Repository.ResidentRepository;
 import org.jspecify.annotations.NonNull;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.Authentication;
@@ -15,7 +17,7 @@ public class AccessValidate {
     @Autowired
     private ContractRepository contractRepository;
     @Autowired
-    private ApartmentRepository apartmentRepository;
+    private ResidentRepository residentRepository;
 
     /**
      * Check xem user có được truy cập vào api của contract không
@@ -33,6 +35,24 @@ public class AccessValidate {
         boolean isAdmin = account.getAuthorities().stream().anyMatch((a) -> a.getAuthority().equals("Contracts_R_01"));
 
         return isAdmin | isOwned;
+    }
+
+    /**
+     * Kiểm tra xem user có quyền xem profile của resident hay không
+     * @param id residentId
+     * @param auth authentication header
+     * @return true nếu được xem, false nếu không được
+     */
+    public boolean canViewResidentProfile(Integer id, @NonNull Authentication auth){
+        Account account = (Account) auth.getPrincipal();
+        Resident resident = residentRepository.findById(id).orElseThrow(() -> new RuntimeException("Resident Id not foud!"));
+        if(account.getRole().getId() == 6){
+            return false;
+        } else if(account.getRole().getId() == 2){
+            Resident residentAccount = residentRepository.findByAccountId(account.getId()).orElse(null);
+            return residentAccount!=null && residentAccount.getId().equals(resident.getId());
+        }
+        return true;
     }
 
     /**
