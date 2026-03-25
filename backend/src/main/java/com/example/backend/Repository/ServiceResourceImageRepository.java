a39f3d8bc4ce712b98003c5eb447f0e2f52edd2e
Tao la Thanh <dthz.bro6@gmail.com>
2026-03-25 09:47:26 +0700

    Revert "Merge branch 'be-thanh' into dev"
    This reverts commit 056f0860376ec9424105aaf3b9c69a02b3f530eb, reversing
    changes made to 8f146dff47252a39c408784511ca6e6363b913f0.

--- backend/src/main/java/com/example/backend/Repository/ServiceResourceImageRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.backend.Repository;
-
-import com.example.backend.Entity.ServiceResourceImage;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ServiceResourceImageRepository extends JpaRepository<ServiceResourceImage, Integer> {
-}
