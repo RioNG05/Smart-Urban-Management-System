8fff556a316b028bb4ab571f7971e3189c2b7cf8
Tao la Thanh <dthz.bro6@gmail.com>
2026-03-27 09:20:47 +0700

    GetInTouch ok

---
+++ backend/src/main/java/com/example/backend/Repository/GetInTouchRepository.java
@@ -0,0 +1,7 @@
+package com.example.backend.Repository;
+
+import com.example.backend.Entity.GetInTouch;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface GetInTouchRepository extends JpaRepository<GetInTouch, Integer> {
+}
